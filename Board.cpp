8e201056c8a6d42995210f21a1f9ad384150d723
davidhh0 <74098365+davidhh0@users.noreply.github.com>
2021-03-24 18:21:38 +0200

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -7,15 +7,15 @@
 #include "Board.hpp"
 using namespace std;
 using std::string;
-const int ROW = 200;
-const int COL = 200;
-const int stringSize=50;
+const unsigned int ROW = 200;
+const unsigned int COL = 200;
+const unsigned int stringSize=50;
 namespace ariel {
 
 		ariel::Board::Board() {
             this->matrix.resize(ROW, vector<char>(COL));
-            for(int i=0;i<ROW;i++){
-                for(int j=0;j<COL;j++){
+            for(unsigned int i=0;i<ROW;i++){
+                for(unsigned int j=0;j<COL;j++){
                     this->matrix[i][j] = '_';
                 }
             }
@@ -26,11 +26,11 @@ namespace ariel {
 		void Board::changeDim() {
 		numberOfChangeDim++;
         this->matrix.resize(numberOfChangeDim * ROW);
-        for(int i=0;i<numberOfChangeDim*ROW;i++){
+        for(unsigned int i=0;i<numberOfChangeDim*ROW;i++){
             this->matrix[i].resize(numberOfChangeDim*COL);
         }
-        for(int i=(numberOfChangeDim-1) * ROW;i<numberOfChangeDim * COL;i++){
-            for(int j=(numberOfChangeDim-1)*COL ; j< numberOfChangeDim*COL;j++){
+        for(unsigned int i=(numberOfChangeDim-1) * ROW;i<numberOfChangeDim * COL;i++){
+            for(unsigned int j=(numberOfChangeDim-1)*COL ; j< numberOfChangeDim*COL;j++){
                 this->matrix[i][j]='_';
             }
         }
@@ -38,7 +38,7 @@ namespace ariel {
         this->isThereChar.resize(numberOfChangeDim * ROW);
 		}
 	
-        void Board::post(int row, int col, Direction d, string str) {
+        void Board::post(unsigned int row, unsigned int col, Direction d, string str) {
         checkThrow(row,col,str.size());
 
         if ((d == Direction::Horizontal && col+str.size()>this->matrix[0].size())
@@ -52,7 +52,7 @@ namespace ariel {
         }
         }
 
-        string Board::read(int row, int col, Direction d, int length) {
+        string Board::read(unsigned int row, unsigned int col, Direction d, unsigned int length) {
             checkThrow(row,col,length);
             if (row >= this->matrix.size() || col >= this->matrix[0].size()) {
             changeDim();
@@ -66,42 +66,42 @@ namespace ariel {
         void Board::show(){
             
         }
-        string Board::readHorizontally(int row, int col, int howmany) {
+        string Board::readHorizontally(unsigned int row, unsigned int col, unsigned int howmany) {
             string str;
-            for (int j = col; j < howmany + col; j++) {
+            for (unsigned int j = col; j < howmany + col; j++) {
                 str += this->matrix[row][j];
             }
              return str;
         }
-        void Board::checkThrow(int row,int col,int length){
+        void Board::checkThrow(unsigned int row,unsigned int col,unsigned int length){
             if(row<0 || col <0 || length>ROW){
                 throw std::invalid_argument("invalid");
 
             }
         }
 
-        string Board::readVertically(int row, int col, int howmany) {
+        string Board::readVertically(unsigned int row, unsigned int col, unsigned int howmany) {
             string str;
-            for (int i = row; i < row + howmany; i++) {
+            for (unsigned int i = row; i < row + howmany; i++) {
                 str += this->matrix[i][col];
                 str += '\n';
             }
             return str;
         }
 
-        void Board::insertHorizonially(int row, int col, string str) {
-            int stringIndex = 0;
+        void Board::insertHorizonially(unsigned int row, unsigned int col, string str) {
+            unsigned int stringIndex = 0;
             this->isThereChar[row] = true;
-            for (int j = col; j < str.size() + col; j++) {
+            for (unsigned int j = col; j < str.size() + col; j++) {
                 this->matrix[row][j] = str[stringIndex++];
             
             }
         }
 
-        void Board::insertVertically(int row, int col, string str) {
-            int stringIndex = 0;
+        void Board::insertVertically(unsigned int row, unsigned int col, string str) {
+            unsigned int stringIndex = 0;
             this->isThereChar[row] = true;
-            for (int i = row; i < str.size() + row; i++) {
+            for (unsigned int i = row; i < str.size() + row; i++) {
                 this->matrix[i][col] = str[stringIndex++];
             }
 
